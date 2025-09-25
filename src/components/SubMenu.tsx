de8e128b3c43122d3bd6ab8ac90f96f9cddbce4d
Coca4017 <jding40@myseneca.ca>
2025-09-24 20:30:27 -0400

    minor update

--- src/components/SubMenu.tsx
+++ src/components/SubMenu.tsx
@@ -13,7 +13,7 @@ const SubMenu:FC = ():JSX.Element=>{
                 to="/books/wishlist"
                 className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
             >
-                ❤️ Wishlist
+                💖 Wishlist
             </Link>
 
             {(userRole === "admin"|| userRole === "owner") && <Link
