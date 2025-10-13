59fd4f34a748525d30848b4f518ffa9b5934974a
Coca4017 <jding40@myseneca.ca>
2025-10-13 17:09:25 -0400

    refactor books page via @tanstack/react-query

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './main.css'
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 
 import AppRoutes from "./router";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <AppRoutes />
+      <QueryClientProvider client={queryClient}>
+        <AppRoutes />
+      </QueryClientProvider>
   </StrictMode>,
 )
--- src/pages/Books/index.tsx
+++ src/pages/Books/index.tsx
@@ -1,93 +1,97 @@
-import { type FC, useEffect, useState } from "react";
+import { type FC,  useState, useMemo} from "react";
 import BookCard from "../../components/BookCard.tsx";
 import bookApi from "../../services/bookApi.ts";
-import {type IBook, type ICategoriedBooks } from "../../types";
+import { type IBook, type ICategoriedBooks } from "../../types";
 import useGetCategoriedBooksFromBooks from "../../hooks/useGetCategoriedBooksFromBooks.ts";
 import SubMenu from "../../components/SubMenu.tsx";
-import {getUserRole} from "../../utils";
+import { getUserRole } from "../../utils";
+
+import { useQuery } from "@tanstack/react-query";
+
+const BooksPage: FC = () => {
+
+    const [keyWord, setKeyWord] = useState<string>("");
+
+    const { data: books = [] } = useQuery({
+        queryKey: ["books"],
+        queryFn: bookApi.getAll,
+    });
 
 
-const BooksPage: FC =() => {
-        const [loading, setLoading] = useState(false);
-        const [books, setBooks] = useState<IBook[]>([]);
-        const [keyWord, setKeyWord] = useState<string>("");
-        // const [filteredBooks, setFilteredBooks] = useState<IBook[]>([]);
-        console.log(loading);
-
-        // useEffect(() => {
-        //         const fetchBooks = async () => {
-        //                 setLoading(true);
-        //                 try {
-        //                         const res:IBook[] = await bookApi.getAll();
-        //                         setBooks(res.filter((book)=>book.title.toLowerCase().includes(keyWord.trim().replace(/\s+/g, " "))));
-        //
-        //                 } catch (err) {
-        //                         console.error("❌ Failed to fetch borrow records:", err);
-        //                 } finally {
-        //                         setLoading(false);
-        //                 }
-        //         };
-        //         fetchBooks();
-        // }, [keyWord]);
-
-        useEffect(() => {
-                const fetchBooks = async () => {
-                        setLoading(true);
-                        try {
-                                const res: IBook[] = await bookApi.getAll();
-                                setBooks(res.filter((book:IBook)=> book.qtyOwned>0));
-                        } catch (err) {
-                                console.error("❌ Failed to fetch books:", err);
-                        } finally {
-                                setLoading(false);
-                        }
-                };
-                fetchBooks();
-        }, []);
-
-        //const categories:Set<string> = useGetCategoriesFromBooks(books);
-        //const categoriedBooks:ICategoriedBooks = useGetCategoriedBooksFromBooks(books);
-
-        const filteredBooks = books.filter(book =>
-            book.title.toLowerCase().includes(keyWord.toLowerCase().trim())
+    const filteredBooks = useMemo(() => {
+        const keyword = keyWord.trim().toLowerCase();
+        if (!keyword) return books.filter((book) => book.qtyOwned > 0);
+        return books.filter(
+            (book) => book.qtyOwned > 0 && book.title.toLowerCase().includes(keyword)
         );
-        const categoriedBooks: ICategoriedBooks = useGetCategoriedBooksFromBooks(filteredBooks);
-        console.log("categoriedBooks: ",categoriedBooks);
+    }, [keyWord, books]);
 
-        const handleKeyWordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-                setKeyWord(e.target.value);
-                // const filteredBooks:IBook[] = books.filter((book:IBook)=> book.title.toLowerCase().includes(keyWord.trim().replace(/\s+/g, " ")));
-                // setBooks(filteredBooks);
-        };
+    const categoriedBooks: ICategoriedBooks =
+        useGetCategoriedBooksFromBooks(filteredBooks);
+    console.log("categoriedBooks: ", categoriedBooks);
 
-        const handleDelete = async(isbn:string): Promise<void>=>{
+    const handleKeyWordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setKeyWord(e.target.value);
+    };
 
-                const confirmed:boolean = window.confirm("Are you sure to delete this book?")
-                if(!confirmed) return;
-                await bookApi.remove(isbn);
-                setBooks(books.filter((book:IBook):boolean=>book.ISBN !== isbn))
-        }
+    const handleDelete = async (isbn: string): Promise<void> => {
+        const confirmed: boolean = window.confirm(
+            "Are you sure to delete this book?"
+        );
+        if (!confirmed) return;
+        await bookApi.remove(isbn);
+        //setBooks(books.filter((book: IBook): boolean => book.ISBN !== isbn));
+    };
 
-        return (
-            <div className="">
-                    <SubMenu />
-                    <div className={"relative"}>
-                            <input type={"text"} className={"border-2 border-blue-800  w-full my-2 h-10 px-3 rounded-xl" } placeholder="Please input title..." value={keyWord} onChange={handleKeyWordChange} />
-                            {keyWord && <span className={"absolute right-3 top-3.5 text-gray-500"}> {filteredBooks.length} {filteredBooks.length>1?"results":"result"}</span>}
-                    </div>
-            {Object.entries(categoriedBooks).map(entry=>{
-                    return <div key={entry[0]}>
-                            <h1 key={entry[0]} className={"my-4 py-2 ps-2 rounded-md bg-blue-700 text-white font-[Grenze_Gotisch] text-2xl"}>{entry[0]}</h1>
-                            <div className="grid gap-4 grid-cols-1 lg:grid-cols-2 3xl:grid-cols-3">
-                                    {entry[1].map((book:IBook)=>{
-                                            return <BookCard book={book} userRole={getUserRole()} key={book.ISBN} onDelete={handleDelete} />
-                                    })}
-                            </div>
-                    </div>
-            })}
+    return (
+        <div className="">
+            <SubMenu />
+            <div className={"relative"}>
+                <input
+                    type={"text"}
+                    className={
+                        "border-2 border-blue-800  w-full my-2 h-10 px-3 rounded-xl"
+                    }
+                    placeholder="Please input title..."
+                    value={keyWord}
+                    onChange={handleKeyWordChange}
+                />
+                {keyWord && (
+                    <span className={"absolute right-3 top-3.5 text-gray-500"}>
+            {" "}
+                        {filteredBooks.length}{" "}
+                        {filteredBooks.length > 1 ? "results" : "result"}
+          </span>
+                )}
             </div>
-
-        );
+            {Object.entries(categoriedBooks).map((entry) => {
+                return (
+                    <div key={entry[0]}>
+                        <h1
+                            key={entry[0]}
+                            className={
+                                "my-4 py-2 ps-2 rounded-md bg-blue-700 text-white font-[Grenze_Gotisch] text-2xl"
+                            }
+                        >
+                            {entry[0]}
+                        </h1>
+                        <div className="grid gap-4 grid-cols-1 lg:grid-cols-2 3xl:grid-cols-3">
+                            {entry[1].map((book: IBook) => {
+                                return (
+                                    <BookCard
+                                        book={book}
+                                        userRole={getUserRole()}
+                                        key={book.ISBN}
+                                        onDelete={handleDelete}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default BooksPage;
